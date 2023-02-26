40082e703fc86bd9199cbe570bef921c3f9fa5ea
Maksym Maksimov <maksymmaksimov88@gmail.com>
2023-02-26 15:25:26 +0800

    Installed black module and reformatted manage.py, company_webpage and webpage_app

--- webpage_app/apps.py
+++ webpage_app/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class WebpageAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'webpage_app'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "webpage_app"
