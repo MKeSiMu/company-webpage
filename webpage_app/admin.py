59eb9dc0c2ac52da01e7cd0258795e24c009191f
Maksym Maksimov <maksymmaksimov88@gmail.com>
2023-02-27 22:10:02 +0800

    Added models and customized admin panel

--- webpage_app/admin.py
+++ webpage_app/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from webpage_app.models import Purchaser, Manufacturer, BearingCategory, BearingType
+
+
+@admin.register(Purchaser)
+class PurchaserAdmin(UserAdmin):
+    pass
+
+
+@admin.register(Manufacturer)
+class PurchaserAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+    list_filter = ("produce_bearing_type", "responsible_purchaser")
+
+
+admin.site.register(BearingType)
+
+admin.site.register(BearingCategory)
+
+admin.site.unregister(Group)
